eb9bc8283ba7a5dc607c8b9ce3359222341188b3
Oleg <sunskays@gmail.com>
2021-03-01 11:16:37 +0200

    Ver-2 (Spiner and Error)

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import ListItem from '../ListItem/ListItem'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Loader from 'react-loader-spinner';
+import ListItem from '../ListItem/ListItem';
+import './List.css';
 
-function List({repos}) {
-    return (
-        <ul>
-            {repos.map((repo) => {
-            return (<ListItem key={repo.id}
-            {...repo}/>)
-            })}
-        </ul>
-    )
+function List({ repos, loader, error }) {
+  return (
+    <ul className="list">
+      {error && <h1>Something went wrong</h1>}
+
+      {loader ? (
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={300}
+          width={300}
+          timeout={3000} //3 secs
+        />
+      ) : (
+        repos.map(repo => {
+          return <ListItem key={repo.id} {...repo} />;
+        })
+      )}
+      {/* Loader - Для ожидания загрузки информации с бэкэнда */}
+      {/* {repos.length > 0 ? 
+          (repos.map(repo => {
+          return <ListItem key={repo.id} {...repo} />;})) 
+          : (<Loader
+          type="Puff"
+          color="#00BFFF"
+          height={300}
+          width={300}
+          timeout={3000} //3 secs
+        />
+      )} */}
+    </ul>
+  );
 }
 
-List.propTypes = {
+List.propTypes = {};
 
-}
-
-export default List
-
+export default List;
--- src/Components/ListItem/ListItem.js
+++ src/Components/ListItem/ListItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import "./ListItem.css"
 
-function ListItem({name}) {
+function ListItem({full_name, owner}) {
+    const{avatar_url, login, type}=owner
     return (
-        <li>
-            <h2>{name}</h2>
+        <li className="card">
+            <img className="card-img" src={avatar_url}></img>
+            <h4 className="card-title">{full_name}</h4>
+            <p className="card-text">{login}</p>
+            <p className="card-text">{type}</p>
         </li>
     )
 }
